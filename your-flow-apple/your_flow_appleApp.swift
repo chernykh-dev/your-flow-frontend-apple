14eeea609cc040b4f3c7346d07d3969c8b239100
Artyom Chernykh <dev.chernykh@yandex.ru>
2025-07-12 19:13:58 +0300

    Initial Commit

---
+++ your-flow-apple/your_flow_appleApp.swift
@@ -0,0 +1,17 @@
+//
+//  your_flow_appleApp.swift
+//  your-flow-apple
+//
+//  Created by Artyom Chernykh on 12.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct your_flow_appleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
